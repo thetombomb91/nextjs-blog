a1537f47e8b260fbb74cc0b34a43850e5d5e2d5f
Thomas Desmond <thomasdes533@gmail.com>
2021-02-16 16:52:01 -0800

    Cleaning up syntax highlighting and using react-syntax-highlight

--- components/codeblock.js
+++ components/codeblock.js
@@ -1,39 +1,21 @@
 import React from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { tomorrow } from "react-syntax-highlighter/dist/cjs/styles/prism"
-import Highlight from "react-highlight.js"
 
 const CodeBlock = ({ language, value }) => {
   return (
-    // <SyntaxHighlighter
-    //   language={language}
-    //   style={tomorrow}
-    //   wrapLines={true}
-    //   showLineNumbers={true}
-    // >
-    //   {value}
-    // </SyntaxHighlighter>
-    /*{ <SyntaxHighlighter 
-       language={language} 
-       style={tomorrow} 
-       wrapLines={true} 
-       showLineNumbers={true}
-     >
-       {value}
-     </SyntaxHighlighter> }*/
-    //  <span>
-    /* <div className="w-screen flex align-center justify-center">
-      <div className="sm:w-3/5 w-11/12 bg-white flex justify-center align-center m-12"> */
     <div className="w-full flex align-center justify-center">
-      <div className="w-full bg-white flex justify-center align-center m-2">
-        <Highlight language={language}>
+      <div className="w-full bg-white flex justify-center align-center m-2 text-sm">
+        <SyntaxHighlighter
+          language={language}
+          style={tomorrow}
+          wrapLines={true}
+          showLineNumbers={true}
+        >
           {value}
-        </Highlight>
+        </SyntaxHighlighter>
       </div>
     </div>
-    /* </div>
-  </div>
-  </span> */
   )
 }
 
