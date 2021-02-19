8ee8c1e2dd60c785f65a5a0ac528f1ad1b2e3c97
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-02-18 18:01:19 -0800

    Added in google analytics. Need to verify it works in production

---
+++ pages/_document.js
@@ -0,0 +1,42 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=%your code here%" >
+                    </script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-101030398-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-101030398-1');
+</script>
+              `,
+                        }}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
