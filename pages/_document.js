6c16539b43ae383eefed1c3665c2199b155647f1
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-02-19 12:04:29 -0800

    Hopefully final try for getting analytics with page navigation tracking working

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <script
                         async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                        src={`https://www.googletagmanager.com/gtag/js?id=UA-101030398-1`}
                     />
                     <script
                         dangerouslySetInnerHTML={{
@@ -22,7 +22,7 @@ class MyDocument extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
+            gtag('config', 'UA-101030398-1', {
               page_path: window.location.pathname,
             });
           `,
