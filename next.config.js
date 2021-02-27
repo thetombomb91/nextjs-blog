9f389dfc64f62e8d43755045ab7199f8442e31a9
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-02-26 16:13:24 -0800

    Added support for bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({})
+
 module.exports = {
     async redirects() {
       return [
