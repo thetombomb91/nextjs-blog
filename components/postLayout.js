4f96e394dd9b13e3eb2f0571b417845e073d32f2
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-03-02 06:27:04 -0800

    Changing defeault og:image for posts.

--- components/postLayout.js
+++ components/postLayout.js
@@ -12,9 +12,7 @@ export default function PostLayout({ children }) {
         <link rel="icon" href="/bombFavicon.ico" />
         <meta
           property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`https://thetombomb-public.s3.amazonaws.com/TomBombHeader.jpeg`}
         />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
