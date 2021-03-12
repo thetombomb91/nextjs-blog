de4ccec2b74f425a31e434ac990d3e51c9dad360
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-03-12 12:50:54 -0800

    Adding redirect for now page

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ module.exports = {
   async redirects() {
     return [
       {
+        source: '/now',
+        destination: '/posts/now',
+        permanent: true,
+      },
+      {
         source: '/2019/11/13/app-module-vs-core-module-vs-shared-module-vs-feature-modules-in-angular',
         destination: '/posts/app-core-shared-feature-modules',
         permanent: true,
