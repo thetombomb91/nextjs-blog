6bcba38e0810eb530b4f5f8c2541b9254c858c86
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-06-15 13:59:37 -0700

    Edits for conformance

--- components/headerLayout.js
+++ components/headerLayout.js
@@ -1,37 +1,33 @@
+import Link from 'next/link'
+
 export default function HeaderLayout() {
   return (
     <>
       <div>
         <header className="text-gray-600 body-font bg-indigo-700">
           <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <a
-              href="/"
-              className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 no-underline	"
-            >
-              <span className="ml-3 text-xl text-white">
-                {" "}
-                <span className="text-white" id="name">Thomas aka</span> TheTomBomb
-              </span>
-            </a>
+            <Link href="/">
+              <a className="flex title-font font-medium items-center text-xl text-white mb-4 md:mb-0 no-underline">
+                Thomas aka TheTomBomb
+              </a>
+            </Link>
             <nav className="md:ml-auto flex flex-wrap items-center  justify-center flex-col sm:flex-row">
-              <a
-                href="/"
-                className="mr-5 text-white hover:text-gray-900 no-underline"
-              >
-                🏡 Home
-              </a>{" "}
-              <a
-                href="/posts/bio"
-                className="mr-5 ml-5 mt-6 sm:mt-0 text-white hover:text-gray-900 no-underline"
-              >
-                🧔 Bio
-              </a>
-              <a
-                href="/events"
-                className="mr-5 ml-5 mt-6 sm:mt-0 text-white hover:text-gray-900 no-underline"
-              >
-                🥑 Speaking Events
-              </a>
+              <Link href="/">
+                <a className="mr-5 text-white hover:text-gray-400 hover:no-underline hover:cursor-pointer">
+                  🏡 Home
+                </a>
+              </Link>
+              {" "}
+              <Link href="/posts/bio">
+                <a className="mr-5 ml-5 mt-6 sm:mt-0 text-white hover:text-gray-400 hover:no-underline hover:cursor-pointer">
+                  🧔 Bio
+                </a>
+              </Link>
+              <Link href="/events">
+                <a className="mr-5 ml-5 mt-6 sm:mt-0 text-white hover:text-gray-400 hover:no-underline hover:cursor-pointer">
+                  🥑 Speaking Events
+                </a>
+              </Link>
             </nav>
           </div>
         </header>
