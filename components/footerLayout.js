71e7fdd0e5a1aef83aa4fa4f62106fe9d164b45f
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-07-06 11:09:28 -0700

    Fix eslint error

--- components/footerLayout.js
+++ components/footerLayout.js
@@ -12,7 +12,7 @@ export default function FooterLayout({ children }) {
                     </Link> */}
                 </div>
                 <div className="text-white font-bold p-4 text-xl invisible md:visible">
-                    <div className="flex justify-center align-center">You've made it this far,</div>
+                    <div className="flex justify-center align-center">You&apos;ve made it this far,</div>
                     <div className="flex flex-wrap justify-center align-center">
                         How about reaching out on Twitter&nbsp;<Link href="https://twitter.com/ThomasJDesmond"><a className="text-white">@ThomasJDesmond</a></Link>&nbsp;or&nbsp;  
                         <Link href="https://www.linkedin.com/in/thomas-desmond-9074475b/"><a className="text-white">LinkedIn!</a></Link>!
